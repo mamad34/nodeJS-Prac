c022b743d6d8fff5286c1e0ee9e89b2859770948
Mamad <mohammadtt77@yahoo.com>
2022-01-09 01:41:56 +0330

    ep 6 sea 5

--- 3/app.js
+++ 3/app.js
@@ -1,11 +1,23 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
-
-server.listen(4000, () => {
-  console.log("SERVER READY AT PORT 4000");
+// use allow us to add a middleware func
+// func inside the app.use will execute for every incoming req
+// func recieve 3 args
+app.use((req, res, next) => {
+  console.log("in the middleWare ");
+  next(); // Alows the request to continue to the next middleWare in line
 });
+app.use((req, res, next) => {
+  console.log("in the second middleWare ");
+  // instead of setHeader & Write we use send (allow us to send a respons)
+  res.send("<h1>Hello from express</h1>");
+});
+
+// const server = http.createServer(app);
+// server.listen(4000, () => {
+//   console.log("SERVER READY AT PORT 4000");
+// });
+
+app.listen(3000);
