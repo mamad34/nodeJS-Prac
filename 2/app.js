0334bbd3bc84770d76b903ed9a5de7272a74a7d6
Mamad <mohammadtt77@yahoo.com>
2022-01-08 17:58:58 +0330

    end of season 3

--- 2/app.js
+++ 2/app.js
@@ -1,5 +1,6 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
+
 // function rqListener(req, res) {}
 // http.createServer(rqListener); // rqlistenr will run for every req
 
@@ -7,47 +8,7 @@ const fs = require("fs");
 
 // http.createServer(function (req, res) {});
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      '<body> <form action="/message" method="POST"><input name="message" type="text"><button type="submit">Send</button> </form> </body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      // on is listening to data event
-      console.log(chunk); // output : <Buffer 6d 65 73 73 61 67 65 3d 77 71 65 71 65>
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      // happen when its done parsing the incoming req data
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody); // outPut : message=wqeqe // message is becuse  we name the input to message
-      const message = parsedBody.split("=")[1];
-      // fs.writeFileSync("message.txt", message); // next lines will run after SyncFileWrite happen
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302; // status code of 302 is for redirection
-        res.setHeader("Location", "/");
-        return res.end();
-        // (err) func will run after we complete writing the file
-      });
-    });
-  }
-  console.log(req.url, req.method, req.headers);
-  // process.exit();
-  res.setHeader("Content-Type", "text/html"); // tell the browser
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body> <h1>Hello from Node JS </h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const server = http.createServer(routes);
+// run the function stored in routes for incoming req
 
 server.listen(3000);
---
+++ 2/routes.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body> <form action="/message" method="POST"><input name="message" type="text"><button type="submit">Send</button> </form> </body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      // on is listening to data event
+      console.log(chunk); // output : <Buffer 6d 65 73 73 61 67 65 3d 77 71 65 71 65>
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      // happen when its done parsing the incoming req data
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody); // outPut : message=wqeqe // message is becuse  we name the input to message
+      const message = parsedBody.split("=")[1];
+      // fs.writeFileSync("message.txt", message); // next lines will run after SyncFileWrite happen
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302; // status code of 302 is for redirection
+        res.setHeader("Location", "/");
+        return res.end();
+        // (err) func will run after we complete writing the file
+      });
+    });
+  }
+  console.log(req.url, req.method, req.headers);
+  // process.exit();
+  res.setHeader("Content-Type", "text/html"); // tell the browser
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body> <h1>Hello from Node JS </h1></body>");
+  res.write("</html>");
+  res.end();
+};
+
+//export this func
+
+module.exports = requestHandler;
+// // or
+// module.exports = {
+//   handler: requestHandler,
+//   someText: "Some Hard coded text",
+// };
+// // or
+// module.exports.handler = requestHandler;
+// module.exports.someText = "some dummy text ";
